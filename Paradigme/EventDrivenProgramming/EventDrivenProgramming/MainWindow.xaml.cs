30d3bafaabef99d5e72c826738557e9ef88932a9
Danciu, Gabriel Mihail (CT RDA BAM CON-RO) <gabriel.danciu@siemens.com>
2018-10-04 17:11:40 +0300

    first commit of first lab

---
+++ Paradigme/EventDrivenProgramming/EventDrivenProgramming/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace EventDrivenProgramming
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.MouseUp += new MouseButtonEventHandler(MouseUpEvent);
+        }
+        private void MouseUpEvent(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Clicked at X:" + e.GetPosition(this).X + " Y:"  + e.GetPosition(this).Y);
+        }
+        private delegate void UpdateSomeControl(TextBlock control, String value);
+
+        private void UpdateTextControl(TextBlock control, String value)
+        {
+            var dispatcher = Application.Current.MainWindow.Dispatcher;
+
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                control.Text = value;
+                System.Threading.Thread.Sleep(1000);
+            }), DispatcherPriority.Background);
+
+            control.Text += " AfterUpdate";
+            
+
+        }      
+
+        private void StackPanel_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateTextControl(txt2, "Click event is bubbled to StackPanel");            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateTextControl(txt1, "Button clicked");            
+        }
+
+        private void Window_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateTextControl(txt3, "Click event is bubbled to Window");
+            
+        }
+    }
+}
---
+++ Paradigme/EventDrivenProgramming/WPFCustomRoutedEvent/EventProgram.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFCustomRoutedEvent
+{
+    public delegate string MyDelegate(string str);
+
+    class EventProgram
+    {
+        event MyDelegate MyEvent;
+        string composer = "";
+        public EventProgram()
+        {
+            this.MyEvent += new MyDelegate(this.Welcome);
+            this.MyEvent += new MyDelegate(this.ReadFile);
+        }
+
+        public string callEvent(String s)
+        {
+            composer = "";
+            composer += MyEvent(s);
+            return composer;
+        }
+
+
+        private string Welcome(string user)
+        {
+            return "Welcome " + user;
+        }
+
+        private string ReadFile(string filepath)
+        {
+            StringBuilder strb = new StringBuilder();
+            StreamReader reader = new StreamReader(filepath);
+            String s;
+            while ((s = reader.ReadLine()) != null)
+            {
+                strb.Append(s);
+            }
+            return strb.ToString();
+        }
+    }
+}
