b2fa9d0446e2ce157334c5976c6c5bc2bc83771a
Danciu, Gabriel Mihail (CT RDA BAM CON-RO) <gabriel.danciu@siemens.com>
2018-10-05 18:33:59 +0300

    added delegates

---
+++ Paradigme/EventDrivenProgramming/DelegateDemo/Delegate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateDemo
+{
+    class Delegate
+    {
+        public delegate int AggMEthod(int firstNumber, int secondNumber);
+        public delegate bool AnotherMethod(int a, int b);
+
+    }
+}
---
+++ Paradigme/EventDrivenProgramming/DelegateDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DelegateDemo.Delegate;
+
+namespace DelegateDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            int resultSum = computeAggregateValue(numbers, sum);
+            Console.WriteLine("Result for sum: {0}", resultSum.ToString());
+            int resultMin = computeAggregateValue(numbers, min);
+            Console.WriteLine("Result for min: {0}", resultMin.ToString());
+
+        }
+        private static int computeAggregateValue(int[] values, AggMEthod method)
+        {
+            int currentValue = values[0];
+            for (int i = 1; i < values.Length; i++)
+            {
+                currentValue = method(currentValue, values[i]);
+            }
+            return currentValue;
+        }
+
+        private static int min(int a, int b)
+        {
+            return a < b ? a : b;
+        }
+
+        private static int sum(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+}
